cbb9b95540b50a1be3e8736c47c03c999c02cc07
Matthew Minton <matthew.minton@infineon.com>
2025-03-05 10:45:04 -0600

    added rest API framework module and tests for same module

--- src/rest_api.py
+++ src/rest_api.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
+from typing import Tuple, List, Dict
+
 api_url = "https://jsonplaceholder.typicode.com"
 
-def get(url: str, payload: str) -> str:
+def get(url: str, payload: str) -> tuple[int, str]:
+    status: int = 0
     packed_url = url + payload
     response = requests.get(packed_url)
+    status = response.status_code
     json_data = response.json()
-    return json.dumps(json_data)
+    return status, json.dumps(json_data)
 
 
 def put(url: str, payload: str) -> str:
@@ -30,6 +34,8 @@ def pretty_print(json_data: str):
 
 if __name__ == "__main__":
     get_payload = "/users/1"
-    get_result = get(api_url, get_payload)
+    http_status: int
+    http_status, get_result = get(api_url, get_payload)
     print("REST API result:")
+    pretty_print(get_result)
     print("Done.")
--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -1,8 +1,17 @@
 import pytest
+import src.rest_api
 
 
-def test_get():
-    assert False
+@pytest.fixture
+def test_payload():
+    return "/users/1"
+
+
+def test_get(test_payload):
+    http_status: int
+    get_payload = test_payload
+    http_status, get_result = src.rest_api.get(src.rest_api.api_url, get_payload)
+    assert http_status != 0
 
 
 def test_put():
